0100bc563c15bd50b8d48bfbc6d5f923650e3f6d
PoornaRakaraddi <rakaraddipoorna@gmail.com>
2022-07-25 17:19:06 +0530

    nine comment

--- src/main/java/GitBatch/GitBatchPractice.java
+++ src/main/java/GitBatch/GitBatchPractice.java
@@ -3,9 +3,7 @@ package GitBatch;
 public class GitBatchPractice {
 	public static void main(String[]arg) {
 System.out.println("poorna");
-System.out.println(" Hi vijay hello hi hello");
-System.out.println("India");
-System.out.println("Bagalkot");
+System.out.println("vijay");
 
 }
 }
