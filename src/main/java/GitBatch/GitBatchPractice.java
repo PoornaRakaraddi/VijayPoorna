c43fbb45741302cbaee2f9447d9791c93bb38660
PoornaRakaraddi <rakaraddipoorna@gmail.com>
2022-07-25 16:03:48 +0530

    first commit

---
+++ src/main/java/GitBatch/GitBatchPractice.java
@@ -0,0 +1,11 @@
+package GitBatch;
+
+public class GitBatchPractice {
+	public static void main(String[]arg) {
+System.out.println("poorna");
+System.out.println(" Hi vijay hello hi hello");
+System.out.println("India");
+System.out.println("Bagalkot");
+
+}
+}
